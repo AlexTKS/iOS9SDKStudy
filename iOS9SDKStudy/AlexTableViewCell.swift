a61e48354984380ad5b9a9dd33b1feaad38f3883
AlexTKS <Alex@aleksandrs-imac.local>
2016-09-12 00:15:05 +0300

    Добавление корневого TableView

---
+++ iOS9SDKStudy/AlexTableViewCell.swift
@@ -0,0 +1,39 @@
+//
+//  AlexTableViewCell.swift
+//  FirstTV
+//
+//  Created by Александр on 22.05.16.
+//  Copyright © 2016 Александр. All rights reserved.
+//
+
+import UIKit
+
+class AlexTableViewCell: UITableViewCell {
+    
+    var restaurant = Restaurant.init(name: "", type: "", location: "", phoneNumber: "", image: "", isVisited: false)
+
+    @IBOutlet weak var RestImage: UIImageView!
+    @IBOutlet weak var Name: UILabel!
+    @IBOutlet weak var Location: UILabel!
+    @IBOutlet weak var Type: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    func fill() -> Void
+    {
+        self.Name.text = restaurant.Name
+        self.Type.text = restaurant.Type
+        self.Location.text = restaurant.Location
+        self.RestImage.image = UIImage.init(named: restaurant.Image)
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ iOS9SDKStudy/classRestaurants.swift
@@ -0,0 +1,27 @@
+//
+//  classRestaurants.swift
+//  FirstTV
+//
+//  Created by Александр on 27.06.16.
+//  Copyright © 2016 Александр. All rights reserved.
+//
+
+import Foundation
+
+class Restaurant {
+    var Name = ""
+    var Type = ""
+    var Location = ""
+    var Image = ""
+    var IsVisited = false
+    var phoneNumber = ""
+    
+    init(name: String, type: String, location: String, phoneNumber: String, image: String, isVisited: Bool) {
+        self.Name = name
+        self.Type = type
+        self.Location = location
+        self.Image = image
+        self.IsVisited = isVisited
+        self.phoneNumber = phoneNumber
+    }
+}
