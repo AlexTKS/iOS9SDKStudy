77e875a9d61200fce55a70578d451a6dfadaa23e
AlexTKS <Alex@Aleksandrs-iMac.local>
2016-11-05 22:42:22 +0200

    Добавлено окно "О программе" и заготовка окна для iCloud

---
+++ iOS9SDKStudy/AlexAboutWebViewController.swift
@@ -0,0 +1,41 @@
+//
+//  AlexAboutWebViewController.swift
+//  iOS9SDKStudy
+//
+//  Created by Александр on 05.11.16.
+//  Copyright © 2016 Александр. All rights reserved.
+//
+
+import UIKit
+
+class AlexAboutWebViewController: UIViewController {
+
+	@IBOutlet weak var webView: UIWebView!
+	
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+		if let url = URL(string: "http://adobe.com") {
+			let request = URLRequest(url: url)
+			webView.loadRequest(request)
+		}
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
